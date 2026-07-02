c908db20bba77b3b7f1530186e661e14eb006c68
Will Berner <Will.o.berner@gmail.com>
2026-07-02 16:36:23 -0400

    Subpage routing fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
+  .then((appRef) => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get('redirect');
+
+    if (redirect && redirect.startsWith('/') && !redirect.startsWith('//')) {
+      const router = appRef.injector.get(Router);
+      router.navigateByUrl(redirect, { replaceUrl: true }).catch((err) => console.error(err));
+    }
+  })
   .catch((err) => console.error(err));
